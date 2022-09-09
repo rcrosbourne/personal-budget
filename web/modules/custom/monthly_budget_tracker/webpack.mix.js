c88738d5be5d1072fe34726c32070151b9b7c2ac
Rainaldo Crosbourne <rcrosbourne@Rainaldos-MacBook-Pro.local>
2022-09-09 00:18:35 -0500

    tried browser sync

--- web/modules/custom/monthly_budget_tracker/webpack.mix.js
+++ web/modules/custom/monthly_budget_tracker/webpack.mix.js
@@ -1,4 +1,11 @@
 let mix = require('laravel-mix');
 mix.postCss("css/monthly-budget-tracker.css", "css/dist/app.css", [
-    require("tailwindcss"),
-]);
+    require("tailwindcss")]);
+// ]).browserSync({
+//     proxy: 'https://personal-budget.ddev.site',
+//     files: [
+//         './templates/**/*.html.twig',
+//         'js/*.js',
+//         'css/*.css',
+//     ],
+// });
