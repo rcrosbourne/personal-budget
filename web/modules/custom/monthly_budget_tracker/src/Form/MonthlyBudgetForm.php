9fbbd8d2f56b95892db8ac3748780d3279e0eb59
Rainaldo Crosbourne <rcrosbourne@Rainaldos-MacBook-Pro.local>
2022-09-08 22:34:06 -0500

    Updated the add form

--- web/modules/custom/monthly_budget_tracker/src/Form/MonthlyBudgetForm.php
+++ web/modules/custom/monthly_budget_tracker/src/Form/MonthlyBudgetForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\monthly_budget_tracker\Form;
 
+use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -9,6 +10,49 @@ use Drupal\Core\Form\FormStateInterface;
  * Form controller for the monthly budget entity edit forms.
  */
 class MonthlyBudgetForm extends ContentEntityForm {
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+    $form['#attached']['library'][]
+      = 'monthly_budget_tracker/monthly_budget_tracker';
+    $form['field_month']['widget'][0]['value']['#date_part_order'] = [
+      'month',
+      'year',
+    ];
+    // if there is no field month default to today
+    if (empty($this->entity->get('field_month')->value)) {
+      $form['field_month']['widget'][0]['value']['#default_value']
+        = new DrupalDateTime('now');
+    }
+    // Update the summary display for income sources
+    $incomeSourceSummary = $this->entity->getIncomeSourceSummary();
+    for ($i = 0; $i < count($incomeSourceSummary); $i++) {
+      $form['field_monthly_income_sources']['widget'][$i]['top']['type']['label']['#markup']
+        = $incomeSourceSummary[$i]['source'];
+      $form['field_monthly_income_sources']['widget'][$i]['top']['summary']['fields_info']['#summary']['content'][0]
+        = $incomeSourceSummary[$i]['amount'];
+    }
+    // Update the summary display for expenses
+    $expenses = $this->entity->getExpenseSummary();
+    for ($i = 0; $i < count($expenses); $i++) {
+      $form['field_monthly_expenses']['widget'][$i]['top']['type']['label']['#markup']
+        = $expenses[$i]['source'];
+      $form['field_monthly_expenses']['widget'][$i]['top']['summary']['fields_info']['#summary']['content'][0]
+        = $expenses[$i]['amount'];
+    }
+
+    $form['actions']['cancel'] = [
+      '#type'   => 'submit',
+      '#value'  => $this->t("Cancel"),
+      '#submit' => ['::cancel'],
+      '#limit_validation_errors' => [],
+    ];
+    return $form;
+  }
+
+  public function cancel(array $form, FormStateInterface $form_state) {
+    $form_state->setRedirect('view.monthly_budget.page_1');
+  }
 
   /**
    * {@inheritdoc}
@@ -21,7 +65,7 @@ class MonthlyBudgetForm extends ContentEntityForm {
     $message_arguments = ['%label' => $entity->toLink()->toString()];
     $logger_arguments = [
       '%label' => $entity->label(),
-      'link' => $entity->toLink($this->t('View'))->toString(),
+      'link'   => $entity->toLink($this->t('View'))->toString(),
     ];
 
     switch ($result) {
